58c3f31211bd267f9e3967a3bb883044762d7b9b
ImamNazar <mnmimamnazar@gmail.com>
2023-02-01 13:42:08 +0530

    final-commit

--- chatbot.py
+++ chatbot.py
@@ -1,19 +1,32 @@
 API_KEY='Please put your generated secret API key from OpenAI API Keys in this field'
 import openai
-import os
-os.environ['OPENAI_Key']=API_KEY
-openai.api_key=os.environ['OPENAI_Key']
 
-keep_prompting=True
-while keep_prompting:
-   prompt = input('What is your question? Type exit if done:)')
-   if prompt=='exit':
-    keep_prompting=False
+openai.api_key = API_KEY
 
-   else:
-      
-      response=openai.Completion.create(engine='text-davinci-003', prompt=prompt, max_tokens=200)
-      print(response['choices'][0]['text'])
+def generate_response(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=200,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    return response["choices"][0]["text"]
+
+def chatbot():
+    print("Hi, I am a chatbot powered by OpenAI. How can I help you today?")
+    while True:
+        prompt = input("You: ")
+        if prompt.lower() in ["quit", "exit", "bye"]:
+            print("Chatbot: Goodbye! Have a great day.")
+            break
+        response = generate_response(prompt)
+        print(f"Chatbot: {response}")
+
+if __name__ == "__main__":
+    chatbot()
+
 
       #Run the following command to install the official Python bindings: pip install openai
 
